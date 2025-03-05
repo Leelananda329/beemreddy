55df0d371728e3a239a301b2cc62ca01b73cc194
leelananda.m <leelanandareddy988@gmail.com>
2025-03-05 16:13:13 +0530

    removed constants

---
+++ lib/constants.dart
@@ -0,0 +1,44 @@
+
+class Constants{
+
+  static const String description="Beem's offers a comprehensive range of insurance products, tailored to address the diverse needs of individuals and businesses. Our offerings include car and bike insurance for vehicle protection, health insurance to cover medical expenses, and international travel insurance for safe travel abroad. For pet owners, we provide Pet Dog insurance, while Cyber insurance protects against online threats. Doctor Liability insurance is also available, offering coverage specifically for medical professionals against liability risks. We further support businesses with Employee insurance and Term insurance for financial security, as well as Fire insurance to protect property from fire-related damages. With this broad portfolio, we ensure reliable and accessible insurance solutions for a wide range of needs.";
+
+  static const String aboutUS="ABOUT US";
+
+  static const String carInsurance="Car Insurance";
+
+  static const String carInsuranceDec="Car insurance, also referred to as motor or auto insurance, is an insurance policy that provides you financial protection if your vehicle suffers damages due to accidents, natural disasters, or thefts. With a Comprehensive Car Insurance Policy, you will be financially secured in case of any losses to your vehicle due to an unfortunate event. Apart from that, a private car insurance policy also covers third-party liabilities and ensures that you comply with the law of the land. Whether you are looking to buy a new car insurance policy to comply with the legal norms or looking for comprehensive coverage, Beem Reddy Insurance has it all! We offer insurance policies, including private car insurance, third-party car insurance, comprehensive car insurance, and own-damage car insurance online at incredibly low premiums. At Beem Reddy Insurance, you will also be able to enjoy a hassle-free car insurance renewal process.";
+
+  static const String bestInsurance="We're Providing Best Insurance Policies";
+
+  static const String bikeInsuranceHeader="Bike Insurance";
+
+  static const String bikeInsuranceDec="Bike insurance or two-wheeler insurance is an insurance policy that covers you against damages to your two-wheeler due to accidents, natural/man-made disasters, fire, and theft. The motorcycle insurance policy also provides law-mandated coverage against third-party liabilities such as damage to another person’s vehicle/property or injuries/death of a third party caused due to the insured two-wheeler. You can buy insurance for two-wheeler to get coverage for motorcycles, scooters, mopeds, etc.";
+
+  static const String healthInsuranceHeader="Health Insurance";
+
+  static const String healthInsuranceDec="Health insurance, also known as Medical insurance Plans/Policies, is an essential financial tool that covers the cost of medical treatment for injuries, illnesses, and more. From basic hospitalization coverage to comprehensive policies with extra benefits like preventive medical health care and death benefits, there’s a variety of health insurance policies available.Finding the right health plan ensures you & your family stay financially protected when unexpected illness or injury strikes. But why stop there?  Beem Reddy  Health Insurance Plans take it up a notch! With zero deductions on claims, and fantastic built-in add-ons, we provide an extra cushion during medical emergencies.";
+
+  static const String contact="Contact";
+
+  static const String aboutUs="About us";
+
+  static const String home="Home";
+
+  static const String termInsurance="Term Insurance";
+
+  static const String commercialVehicle="Commercial Vehicle Insurance";
+
+  static const String termInsuranceDec="Term insurance is a type of life insurance that provides financial protection for a specified period, ensuring that beneficiaries receive a death benefit if the policyholder passes away during the term. It offers affordable premiums and straightforward coverage, making it an ideal choice for those seeking security for their loved ones without the complexities of permanent life insurance.";
+
+  static const String appPossword="uxjl sshb ozyf mkwk";
+
+  static const String commercialVehicleDec="Commercial Vehicle Insurance offers essential coverage for business vehicles, protecting against accidents, theft, and damages. This insurance safeguards company assets by covering repair costs and minimizing financial loss. It also includes liability protection for third-party injuries or property damage. With 24/7 support and a wide network of repair centers, it ensures that business operations stay on track";
+
+  static const String groupHealthInsurance="Group Health insurance";
+
+  static const String groupHealthInsuranceDec="Group Health Insurance is an insurance policy provided to a group of employees or members working for a company or an organisation. Hence, it is also known as Employee Health Insurance. Here, the employer usually pays the entire premium to offer health insurance as an employee benefit.With this policy, employees can receive financial security during medical uncertainties, and employers can receive tax benefits on the paid premium. A Group Health Insurance Plan can also provide coverage for an employee’s family members (parents, spouse, dependent children).";
+
+  static const String brevoApiKey="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";
+
+}
