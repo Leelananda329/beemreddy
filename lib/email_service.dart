69fd863d9db6f6b6247914f0c3b489b64621c89d
leelananda.m <leelanandareddy988@gmail.com>
2025-03-05 16:06:20 +0530

    Cleared Git cache

--- lib/email_service.dart
+++ lib/email_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'constants.dart';
 
 class EmailService {
-  static  String apiKey = Constants.brevoApiKey; // Replace with your Brevo API Key
+  static  String apiKey = ""; // Replace with your Brevo API Key
 
   static Future<bool> sendEmail({
     required String recipientEmail,
